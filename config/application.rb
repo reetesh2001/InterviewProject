afd9b2b0fc6017e8431d3fa202abdab77fee097e
Reetesh Kumar Pandey <rpandey@gammastack.com>
2024-07-30 20:14:21 +0530

    errors handling

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,7 @@ module Interviewproject
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    config.exceptions_app = self.routes
   end
 end
